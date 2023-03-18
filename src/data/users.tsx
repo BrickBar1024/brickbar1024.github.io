ecd8117aa0660f376627e0f843ceb9906128f8b3
Joy-sudo996 <752266241@qq.com>
2023-03-18 16:09:28 +0800

    update News layout

---
+++ src/data/users.tsx
@@ -0,0 +1,112 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+/* eslint-disable global-require */
+
+import {translate} from '@docusaurus/Translate';
+import {sortBy} from '@site/src/utils/jsUtils';
+
+/*
+ * ADD YOUR SITE TO THE DOCUSAURUS SHOWCASE
+ *
+ * Please don't submit a PR yourself: use the Github Discussion instead:
+ * https://github.com/facebook/docusaurus/discussions/7826
+ *
+ * Instructions for maintainers:
+ * - Add the site in the json array below
+ * - `title` is the project's name (no need for the "Docs" suffix)
+ * - A short (≤120 characters) description of the project
+ * - Use relevant tags to categorize the site (read the tag descriptions on the
+ *   https://docusaurus.io/showcase page and some further clarifications below)
+ * - Add a local image preview (decent screenshot of the Docusaurus site)
+ * - The image MUST be added to the GitHub repository, and use `require("img")`
+ * - The image has to have minimum width 640 and an aspect of no wider than 2:1
+ * - If a website is open-source, add a source link. The link should open
+ *   to a directory containing the `docusaurus.config.js` file
+ * - Resize images: node admin/scripts/resizeImage.js
+ * - Run optimizt manually (see resize image script comment)
+ * - Open a PR and check for reported CI errors
+ *
+ * Example PR: https://github.com/facebook/docusaurus/pull/7620
+ */
+
+// LIST OF AVAILABLE TAGS
+// Available tags to assign to a showcase site
+// Please choose all tags that you think might apply.
+// We'll remove inappropriate tags, but it's less likely that we add tags.
+export type TagType =
+  // DO NOT USE THIS TAG: we choose sites to add to favorites
+  | 'favorite'
+  | 'AI'
+
+// Add sites to this list
+// prettier-ignore
+const Users: User[] = [
+  {
+    title: 'ResNet-RS',
+    description: '谷歌领衔调优ResNet，性能全面超越EfficientNet系列',
+    preview: require('./showcase/ResNet-RS.png'),
+    website: 'https://mp.weixin.qq.com/s/I27DJYnV9RB7P21i9Tt3WQ',
+    source: 'https://arxiv.org/abs/2103.07579',
+    tags: ['AI','favorite'],
+  },
+
+  
+
+
+  /*
+  Pro Tip: add your site in alphabetical order.
+  Appending your site here (at the end) is more likely to produce Git conflicts.
+   */
+];
+
+export type User = {
+  title: string;
+  description: string;
+  preview: string | null; // null = use our serverless screenshot service
+  website: string;
+  source: string | null;
+  tags: TagType[];
+};
+
+export type Tag = {
+  label: string;
+  description: string;
+  color: string;
+};
+
+export const Tags: {[type in TagType]: Tag} = {
+  favorite: {
+    label: translate({message: 'Favorite'}),
+    description: translate({
+      message:
+        'Our favorite Docusaurus sites that you must absolutely check out!',
+      id: 'showcase.tag.favorite.description',
+    }),
+    color: '#e9669e',
+  },
+  AI: {
+    label: translate({message: 'AI'}),
+    description: translate({
+      message: 'AI!',
+      id: 'showcase.tag.AI.description',
+    }),
+    color: '#39ca30',
+  },
+};
+
+export const TagList = Object.keys(Tags) as TagType[];
+function sortUsers() {
+  let result = Users;
+  // Sort by site name
+  result = sortBy(result, (user) => user.title.toLowerCase());
+  // Sort by favorite tag, favorites first
+  result = sortBy(result, (user) => !user.tags.includes('favorite'));
+  return result;
+}
+
+export const sortedUsers = sortUsers();
